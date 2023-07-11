51b7795b20e1c5038e911bd1fafef51a9fc24909
a.sotnichenko <haliivi@mail.ru>
2023-07-11 16:02:34 +0700

    Fill auth component and fill it.

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,10 +1,36 @@
 import { Component } from "react";
+import classes from "./Auth.module.css";
+import Button from "../../components/UI/Button/Button"
 
 export default class Auth extends Component {
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    onSubmitHandler = e => {e.preventDefault()} 
+
     render () {
         return (
-            <div className="">
-                Auth
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form onSubmit={this.onSubmitHandler} className={classes.AuthForm}>
+                        <input type="text" />
+                        <input type="text" />
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                        >Войти</Button>
+                        <Button
+                            type="primary"
+                            onClick={this.registerHandler}
+                        >Зарегистрироваться</Button>
+                    </form>
+                </div>
             </div>
         )
     }

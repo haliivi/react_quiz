f079d7d670652afb210bde070524331576b747c8
a.sotnichenko <haliivi@mail.ru>
2023-07-12 08:35:07 +0700

    1) Create and styling input field. 2) Include it in Auth component.

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,35 @@
+import classes from "./Input.module.css"
+
+function isInvalid ({valid, touched, shouldValidate}) {
+    return !valid && shouldValidate && touched
+}
+
+const Input = props => {
+    const {
+        type='text',
+        cls=[classes.Input],
+        label,
+        value,
+        onChange,
+        errorMessage,
+    } = props
+    const htmlFor = `${type}-${Math.random()}`
+    
+    if (isInvalid(props)) {
+        cls.push(classes.invalid)
+    }
+
+    return (
+        <div className={cls.join(' ')}>
+            <label
+                htmlFor={htmlFor}
+                value={value}
+                onChange={onChange}
+            >{label}</label>
+            <input id={htmlFor} type={type} />
+            {isInvalid(props) ? <span>{errorMessage || "Введите верное значение"}</span> : null}
+        </div>
+    )
+}
+
+export default Input

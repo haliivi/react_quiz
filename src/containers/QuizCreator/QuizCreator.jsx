5addff22c9262e9a2655eeb2f734856a61b61d5a
a.sotnichenko <haliivi@mail.ru>
2023-07-13 15:11:58 +0700

    1) Create draft form of create quiz. 2) Added style component QuizCreator.

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,10 +1,47 @@
-import { Component } from "react"
+import { Component } from "react";
+import classes from "./QuizCreator.module.css"
+import Button from "../../components/UI/Button/Button"
 
 export default class QuizCreator extends Component {
+    onSumbitHandler = e => {
+        e.preventDefault()
+    }
+
+    addQuestionHandler = () => {
+
+    }
+
+    createQuizHandler = () => {
+
+    }
+
     render () {
         return (
-            <div className="">
-                QuizCreator
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Создание теста</h1>
+                    <form onSumbit={e => this.onSumbitHandler(e)}>
+                        <input type="text" />
+                        <hr />
+                        <input type="text" />
+                        <input type="text" />
+                        <input type="text" />
+                        <input type="text" />
+                        <select name="" id=""></select>
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}
+                        >
+                            Добавить вопрос
+                        </Button>
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}
+                        >
+                            Создать тест
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }

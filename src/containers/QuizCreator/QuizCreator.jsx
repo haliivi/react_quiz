7620608dd433ca8e640079afa05888c376576f71
a.sotnichenko <haliivi@mail.ru>
2023-07-14 13:18:35 +0700

    Finish write QuizCreator component.

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -5,6 +5,7 @@ import { createControl } from "../../form/formFramework";
 import Input from "../../components/UI/Input/Input";
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary"
 import Select from "../../components/UI/Select/Select";
+import is from "is_js"
 
 function createOptionControl(number) {
     return createControl(
@@ -43,6 +44,7 @@ export default class QuizCreator extends Component {
         this.props = props
         this.state = {
             quiz: [],
+            isFormValid: false,
             rightAnswerId: 1,
             formControls: createFormControls()
         }
@@ -52,16 +54,66 @@ export default class QuizCreator extends Component {
         e.preventDefault()
     }
 
-    addQuestionHandler = () => {
-
+    addQuestionHandler = e => {
+        e.preventDefault()
+        const quiz = this.state.quiz.concat()
+        const index = quiz.length + 1
+        const {question, option1, option2, option3, option4} = this.state.formControls
+        const questionItem = {
+            question: question.value,
+            id: index,
+            rightAnswerId: this.state.rightAnswerId,
+            answers: [
+                {text: option1.value, id: option1.id},
+                {text: option2.value, id: option2.id},
+                {text: option3.value, id: option3.id},
+                {text: option4.value, id: option4.id},
+            ]
+        }
+        quiz.push(questionItem)
+        this.setState({
+            quiz,
+            isFormValid: false,
+            rightAnswerId: 1,
+            formControls: createFormControls()
+        })
     }
 
-    createQuizHandler = () => {
+    createQuizHandler = e => {
+        e.preventDefault()
+        console.log(this.state.quiz)
+    }
 
+    validateControl (value, validation) {
+        let isValid = true
+        if (!validation) {return isValid}
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+        if (validation.email) {
+            isValid = is.email(value) && isValid
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+        return isValid
     }
 
     changeHandler = (value, controlName) => {
-
+        const formControls = {...this.state.formControls}
+        const control = {...formControls[controlName]}
+        control.value = value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+        formControls[controlName] = control
+        let isFormValid = true
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+        this.setState({
+            formControls,
+            isFormValid
+        })
     }
 
     renderControls () {
@@ -79,7 +131,7 @@ export default class QuizCreator extends Component {
                         shouldValidate={!!control.validation}
                         touched={control.touched}
                         errorMessage={control.errorMessage}
-                        onChange={e => this.changeHandler(e.targe.value, controlName)}
+                        onChange={e => this.changeHandler(e.target.value, controlName)}
                     />
                     {i === 0 ? <hr /> : null}
                 </Auxiliary>
@@ -115,6 +167,7 @@ export default class QuizCreator extends Component {
                         <Button
                             type="primary"
                             onClick={this.addQuestionHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Добавить вопрос
                         </Button>

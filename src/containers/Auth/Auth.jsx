753bd7fd03964ed92881fe5131e5d3627acfb7e1
a.sotnichenko <haliivi@mail.ru>
2023-07-19 08:56:24 +0700

    1) Install package react-jwt. 2) Rewrite Auth container to redux.

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,11 +1,12 @@
-import { Component } from "react";
-import classes from "./Auth.module.css";
-import Button from "../../components/UI/Button/Button"
-import Input from "../../components/UI/Input/Input"
-import is from "is_js";
-import axios from "axios";
+import { Component } from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+import is from 'is_js';
+import { connect } from 'react-redux';
+import { auth } from '../../store/actions/auth';
 
-export default class Auth extends Component {
+class Auth extends Component {
     constructor (props) {
         super(props)
         this.props = props
@@ -41,30 +42,20 @@ export default class Auth extends Component {
         this.api_key = process.env.REACT_APP_API_KEY
     }
 
-    loginHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecretToken: true,
-        }
-        try {
-            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.api_key}`, authData)
-        } catch (e) {
-            console.log(e)
-        }
+    loginHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            true,
+        )
     }
 
-    registerHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecretToken: true,
-        }
-        try {
-            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.api_key}`, authData)
-        } catch (e) {
-            console.log(e)
-        }
+    registerHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            false,
+        )
     }
 
     onSubmitHandler = e => {e.preventDefault()}
@@ -144,3 +135,12 @@ export default class Auth extends Component {
         )
     }
 }
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+        auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
---
+++ src/store/actions/auth.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { decodeToken } from 'react-jwt';
+import { AUTH_SUCCESS, AUTH_LOGOUT } from './actionTypes';
+
+export  function auth(email, password, isLogin) {
+    return async dispatch => {
+        const authData = {
+            email,
+            password,
+            returnSecretToken: true,
+        }
+        const api_key = process.env.REACT_APP_API_KEY
+        const response = await axios.post(
+            isLogin ?
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${api_key}` :
+            `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${api_key}`,
+            authData
+        )
+        const data = response.data
+        const expirationTime = decodeToken(data.idToken).exp * 1000
+        const tokenLiveTime = expirationTime - Date.now()
+        const expirationDate = new Date(expirationTime)
+        localStorage.setItem('token', data.idToken)
+        localStorage.setItem('userId', data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
+        dispatch(authSuccess(data.idToken))
+        dispatch(autoLogout(tokenLiveTime))
+    }
+}
+
+export function autoLogout(time) {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout())
+        }, time)
+    }
+}
+
+export function logout() {
+    ['token', 'userId', 'expirationDate'].forEach((item) => localStorage.removeItem(item))
+    return {
+        type: AUTH_LOGOUT,
+    }
+}
+
+export function authSuccess(token) {
+    return {
+        type: AUTH_SUCCESS,
+        token,
+    }
+}
